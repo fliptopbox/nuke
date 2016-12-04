2dafc5dee62613ef37cd1cdebff1011e479c48bf
Bruce Thomas <response.write@gmail.com>
2016-12-04 02:54:57 +0000

    FIX python milliseconds

--- scripts/mkproxy/assets/make-proxy-build.py
+++ scripts/mkproxy/assets/make-proxy-build.py
@@ -6,7 +6,7 @@ from string import Template
 def version ():
     major = 0
     minor = 3
-    build = 24
+    build = 25
     ver = [str(major), str(minor), str(build)]
     return '.'.join(ver)
 
@@ -88,7 +88,7 @@ def now(style='%Y/%m/%d %H:%M:%S'):
     
     if style == 'epoc':
         # javascript uses millisecons
-        return str(int(time.time()*1000.0))
+        return str(int(round(time.time() * 1000))))
     if style == 'time': style = '%H:%M:%S'
     if style == 'microsecond': style = '%H:%M:%S.%f'
     if style == 'filename': style = '%Y%m%d_%H%M%S'
