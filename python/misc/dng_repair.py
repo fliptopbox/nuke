9de1af47dc18efb87b44e29193c40093ea268879
Bruce Thomas <response.write@gmail.com>
2017-04-15 13:49:06 +0100

    UPDATE DNG repair

--- python/misc/dng_repair.py
+++ python/misc/dng_repair.py
@@ -1,29 +1,30 @@
 import os, sys, re, shutil, argparse
 
-
-
+def strip_trailing_slash(path=''):
+    if not path: return ''
+    return re.sub('[\\\/]+$', '', path)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", type=str, help="(String) Source folder to scan for video assets")
 
 args = parser.parse_args()
 
-
-# folder_root = "/home/bruce/Desktop/BMPCC/ftbx_1_2017-04-06_1719_C0000"
+# check input arguments
 folder_root = args.i or None
-
 if not folder_root:
-	print "require source folder"
+	print "Require source folder"
 	sys.exit(0)
 
-
+# check folder is valid
+folder_root = strip_trailing_slash(folder_root)
 if not os.path.isdir(folder_root):
 	print "Source folder does not exist"
 	sys.exit(0)
 
-
+# derive the base file name, based on folder name
 folder_path = folder_root.split('/')
 file_base = folder_path[-1]
+
 
 print "\n\nBase File Name:\n", file_base, "\n"
 
@@ -36,12 +37,14 @@ max_string = ''
 
 frame_number = re.compile('(.*)_([^\.]+)(\.dng)')
 frame_dictionary = {}
+frame_digits = 6 # default "zero" padding ie. 000001
 
 for root, subdirs, files in os.walk(folder_root):
 	for file in files:
 
 		# work on files in the frame sequence
 		if frame_number.search(file):
+
 			result = frame_number.search(file)
 			current_string = result.group(2)
 			current_frame = int(result.group(2))
@@ -55,18 +58,18 @@ for root, subdirs, files in os.walk(folder_root):
 			# print "%s -- (%s) %s -- %s (%s)" % (file, current_string, current_frame, max_frame, max_string)
 
 print "Summary of missing files:"
-print "Total files:", len(frame_dictionary)
-print "Max frame number:", max_frame
-print "Missing frames:", max_frame - len(frame_dictionary)
+print "Total number of files:", len(frame_dictionary)
+print "Maximum frame number:", max_frame
+print "Dropped frame count:", max_frame - len(frame_dictionary)
 
 missing_files = []
+frame_digits = len(max_string)
 
 for i in range(0,max_frame):
 	if not i in frame_dictionary:
-		# print x+1, 'missing', i, last_frame, "%s_%s.dng" % (file_base, str(i).rjust(6, '0'))
 		
-		src = "%s/%s_%s.dng" % (folder_root, file_base, str(last_frame).rjust(6, '0'))
-		dst = "%s/%s_%s.dng" % (folder_root, file_base, str(i).rjust(6, '0'))
+		src = "%s/%s_%s.dng" % (folder_root, file_base, str(last_frame).rjust(frame_digits, '0'))
+		dst = "%s/%s_%s.dng" % (folder_root, file_base, str(i).rjust(frame_digits, '0'))
 
 		shutil.copyfile(src, dst)
 		missing_files.append(i)
@@ -76,5 +79,4 @@ for i in range(0,max_frame):
 
 	last_frame = i
 
-print "\n\nCreated DNG frames for ..."
-print missing_files
+print "\n\nCreated DNG frames for ...\n", missing_files
